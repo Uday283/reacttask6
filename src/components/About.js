ca1b68fb6d0af598426c05f6ae9348b7546e5d50
SOMA UDAY KIRAN <107768879+Uday283@users.noreply.github.com>
2023-10-14 13:05:46 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>
+        HY THIS IS UDAY
+      </p>
+      
+    </section>
+  );
+};
+
+export default About;
---
+++ src/components/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <section id="projects">
+      <h2>Projects</h2>
+      I am currently working on full-stack projects and am more enthusiastic about data structures and algorithms
+    </section>
+  );
+}
+
+export default Projects;
